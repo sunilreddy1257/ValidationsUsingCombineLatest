290d867befa20058ef2874a8847a484d9a296bee
sunilreddy1257 <sunilreddy1257@gmail.com>
2023-05-22 12:11:20 +0530

    - Using CombineLatest implemented signup validations - Signup button disabling implemented two ways one is using sink and other one is using assign

--- ValidationsUsingCombineLatest/ValidationsUsingCombineLatest/ViewController.swift
+++ ValidationsUsingCombineLatest/ValidationsUsingCombineLatest/ViewController.swift
@@ -12,38 +12,65 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var name: UITextField!
-    @IBOutlet weak var mobileNumber: UITextField!
+    @IBOutlet weak var address: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
     
     @Published private var usernameVal: String = ""
     @Published private var nameVal: String = ""
-    @Published private var mobileNumberVal: String = ""
+    @Published private var addressVal: String = ""
     
+    private var subscriptions = Set<AnyCancellable>()
+    
+    private var validationPublisher: AnyPublisher<Bool, Never> {
+        return Publishers.CombineLatest3($usernameVal, $nameVal, $addressVal)
+            .map { usernameVal, nameVal, addressVal in
+                !usernameVal.isEmpty && !addressVal.isEmpty && !nameVal.isEmpty
+            }
+            .eraseToAnyPublisher()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Using Sink
+        /*validationPublisher.receive(on: RunLoop.main)
+            .sink {[weak self] booleanVal in
+                if booleanVal == true {
+                    self?.signupButton.isEnabled = true
+                } else {
+                    self?.signupButton.isEnabled = false
+                }
+            }
+            .store(in: &self.subscriptions)*/
+        
+        
+        //Using Assign
+        validationPublisher
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.isEnabled, on: signupButton)
+            .store(in: &self.subscriptions)
     }
 
     @IBAction func signupButtonPressed(_ sender: UIButton) {
-        
+        print("enabled button")
+    }
+    /*@IBAction func usernameChanges(_ sender: UITextField) {
+        usernameVal = sender.text ?? ""
+    }*/
+    @IBAction func nameChanges(_ sender: UITextField) {
+        nameVal = sender.text ?? ""
+    }
+    @IBAction func addressChanges(_ sender: UITextField) {
+        addressVal = sender.text ?? ""
+    }
+    @IBAction func usernameChanges(_ sender: UITextField) {
+        usernameVal = sender.text ?? ""
     }
     
 }
 
 //MARK: UITextField Delegate Methods
 extension ViewController: UITextFieldDelegate {
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        switch textField {
-        case username:
-            usernameVal = textField.text ?? ""
-        case name:
-            nameVal = textField.text ?? ""
-        case mobileNumber:
-            mobileNumberVal = textField.text ?? ""
-        default:
-            break
-        }
-        
-    }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
